c1af69a6aa6b6a26fb3c1ba79738b95dfeeefebd
Arun Kumar <kumarkorra99@gmail.com>
2025-03-13 21:28:27 +0530

    c

--- server/config/db.js
+++ server/config/db.js
@@ -1,8 +1,13 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const sequelize = new Sequelize('database', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'postgres' // or 'mysql'
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
+  dialect: 'postgres', // or 'mysql'
+  logging: console.log, // Enable logging
+  dialectOptions: {
+    connectTimeout: 60000 // Increase connection timeout
+  }
 });
 
 module.exports = sequelize;
--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
-const path = require('path'); // Add this line
-const { Sequelize } = require('sequelize');
+const path = require('path');
+const sequelize = require('./config/db'); // Update this line
 const authRoutes = require('./routes/authRoutes');
 const pdfRoutes = require('./routes/pdfRoutes');
 const subscriptionRoutes = require('./routes/subscriptionRoutes');
@@ -12,14 +12,12 @@ app.use(express.json());
 app.use(cors()); // Allow frontend to connect
 
 // Initialize Sequelize
-const sequelize = new Sequelize('database', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'postgres' // or 'mysql'
-});
-
 sequelize.authenticate()
   .then(() => console.log('Database connected'))
-  .catch(err => console.error('Unable to connect to the database:', err));
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+    process.exit(1); // Exit the process with an error code
+  });
 
 app.use('/api/auth', authRoutes);
 app.use('/api/pdfs', pdfRoutes);
@@ -27,10 +25,10 @@ app.use('/api/subscriptions', subscriptionRoutes);
 app.use('/api/products', productRoutes);
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/build'))); // Add this line
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
-app.get('*', (req, res) => { // Add this block
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
